e0952df7279407d6a7f47176c705628346b8bd61
wtoshm <wtoshm@gmail.com>
2025-06-26 22:01:53 +0900

    fix : 버그 수정

--- bin/dart_debug_sample.dart
+++ bin/dart_debug_sample.dart
@@ -2,6 +2,6 @@ import 'package:dart_debug_sample/dart_debug_sample.dart';
 
 void main() {
   YukymController yukymController = YukymController();
-  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
-  print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
+  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시 경오3국
+  print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시 갑자7국
 }
